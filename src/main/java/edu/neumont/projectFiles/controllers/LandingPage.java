f086bf7f6bea67ed6b7956408c6894f79931af5c
Anthony Corbin <dragon1672@gmail.com>
2015-05-18 11:26:44 -0600

    Adding landing page and random SWF getter

---
+++ src/main/java/edu/neumont/projectFiles/controllers/LandingPage.java
@@ -0,0 +1,18 @@
+package edu.neumont.projectFiles.controllers;
+
+import edu.neumont.projectFiles.controllers.routing.Route;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Anthony on 5/18/2015.
+ */
+public class LandingPage {
+    public static Pattern Regex = Pattern.compile("/");
+
+    public static Route DoIt(HttpServletRequest request) {
+        return Route.ForwardToUrl("/WEB-INF/index.jsp");
+    }
+}
