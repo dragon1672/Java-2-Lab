08eee27a38817e5afb8e2262caed7f41108fed91
Brittany Waite <bnwbnw3@gmail.com>
2015-05-20 11:18:16 -0600

    Switching to use the DummyDAL until the Database DAL is written.

--- src/main/java/edu/neumont/projectFiles/services/Singletons.java
+++ src/main/java/edu/neumont/projectFiles/services/Singletons.java
@@ -1,11 +1,10 @@
 package edu.neumont.projectFiles.services;
 
 import edu.neumont.projectFiles.interfaces.DAL;
-import edu.neumont.projectFiles.services.DataBaseDAL;
 
 /**
  * Created by bwaite on 5/19/2015.
  */
 public class Singletons {
-    public static DAL theDAL = new DataBaseDAL();
+    public static DAL theDAL = new DummyDAL();//new DataBaseDAL();
 }
