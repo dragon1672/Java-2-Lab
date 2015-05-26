934ea2e7935d38c4cab9daa27060b3833fe5c875
Brittany Waite <bnwbnw3@gmail.com>
2015-05-26 11:02:07 -0600

    dummy RPS game.

--- src/main/java/edu/neumont/projectFiles/controllers/RockPaperScissorsPage.java
+++ src/main/java/edu/neumont/projectFiles/controllers/RockPaperScissorsPage.java
@@ -13,26 +13,99 @@ import java.util.regex.Pattern;
  */
 public class RockPaperScissorsPage {
     public static Pattern Regex = Pattern.compile("/RPS");
+    public static class QuickGameModel{
+        private UserModel user1;
+        private UserModel user2;
 
-    public static Route getPage(HttpServletRequest request)
-    {
-        Route toReturn = Route.ForwardToUrl("");
+        private String user1Choice;
+        private String user2Choice;
+
+        private String winner;
+
+        public UserModel getUser1() {
+            return user1;
+        }
+
+        public void setUser1(UserModel user1) {
+            this.user1 = user1;
+        }
+
+        public UserModel getUser2() {
+            return user2;
+        }
+
+        public void setUser2(UserModel user2) {
+            this.user2 = user2;
+        }
+
+        public String getUser1Choice() {
+            return user1Choice;
+        }
+
+        public void setUser1Choice(String user1Choice) {
+            this.user1Choice = user1Choice;
+        }
+
+        public String getUser2Choice() {
+            return user2Choice;
+        }
+
+        public void setUser2Choice(String user2Choice) {
+            this.user2Choice = user2Choice;
+        }
+
+        public String getWinner() {
+            if(winner == null)
+            {
+                winner = "FIGURE IT OUT";
+
+            }
+            return winner;
+        }
+    }
+    private static QuickGameModel qgm = new QuickGameModel();;
+
+    public static Route getPage(HttpServletRequest request) {
+        Route toReturn = Route.ForwardToUrl("/WEB-INF/login.jsp");
         String username = "";
         Long userID = null;
         HttpSession session = request.getSession();
-        if(session != null) {
-            username = (String)session.getAttribute("username");
-            userID = (Long)session.getAttribute("userID");
+        if (session != null) {
+            username = (String) session.getAttribute("username");
+            userID = (Long) session.getAttribute("userID");
         }
-        if(userID != null) {
+        //If logged in check for players
+        if (userID != null) {
+            toReturn = Route.ForwardToUrl("/WEB-INF/WaitingForPlayers.jsp");
             UserModel userM = Singletons.theDAL.retrieveUserModel(userID);
-            String choice = request.getParameter("choice");
-            if (userM != null) {
-                if(choice == null || choice.isEmpty()) {
-                    toReturn = Route.ForwardToUrl("/WEB-INF/RPSstart.jsp");
-                } else {
-                    toReturn = Route.ForwardToUrl("/WEB-INF/RPSresults.jsp", choice);
+            //connect user to this game if not connected
+            if (qgm.getUser1() == null) {
+                qgm.setUser1(userM);
+            } else if (qgm.getUser2() == null && qgm.getUser1() != userM) {
+                qgm.setUser2(userM);
+            }
+            //check for 2 players for this game
+            if (qgm.getUser1() != null && qgm.getUser2() != null) {
+                //check weather need to pick choice or display results
+                String choice = request.getParameter("choice");
+                if(userM == qgm.getUser1()) {
+                    qgm.setUser1Choice(choice);
+                } else if(userM ==  qgm.getUser2()) {
+                    qgm.setUser2Choice(choice);
                 }
+                if (userM != null) {
+                    if((qgm.getUser1Choice() != null && qgm.getUser2Choice() == null)
+                        ||(qgm.getUser2Choice() != null && qgm.getUser1Choice() == null)){
+                        toReturn = Route.ForwardToUrl("/WEB-INF/plaintext.jsp", "Refresh to Check for Results");
+                    }else if(qgm.getUser1Choice() != null && qgm.getUser2Choice() != null){
+                        toReturn = Route.ForwardToUrl("/WEB-INF/RPSresults.jsp", qgm);
+                        qgm = new QuickGameModel();
+                    }else{
+                        toReturn = Route.ForwardToUrl("/WEB-INF/RPSstart.jsp");
+                    }
+                }
+            } else {
+                toReturn = Route.ForwardToUrl("/WEB-INF/plaintext.jsp","Refresh to Check for Another player");
             }
         }
         return toReturn;
--- src/main/webapp/WEB-INF/RPSresults.jsp
+++ src/main/webapp/WEB-INF/RPSresults.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:useBean id="model" scope="request" type="edu.neumont.projectFiles.controllers.RockPaperScissorsPage.QuickGameModel"/>
 <html>
 <head>
     <title>RPS</title>
@@ -11,6 +12,9 @@
 <body>
 <jsp:include page="header.jsp" />
 <h2>Rock Paper Scissors</h2>
-<p>Hello <%= session.getAttribute("username") %> You played: ${model}</p>
+<p>
+  Greetings your play session has finished. ${model.user1.displayName} played ${model.user1Choice}, while ${model.user2.displayName} played ${model.user2Choice}.
+</p>
+<h5>The winner is ${model.winner}</h5>
 </body>
 </html>
