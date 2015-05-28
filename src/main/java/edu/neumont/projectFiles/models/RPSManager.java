cf3e4905f0758b95d101998585e6c529bfda2408
Brittany Waite <bnwbnw3@gmail.com>
2015-05-28 10:23:24 -0600

    Made RPSManager it's own file.

---
+++ src/main/java/edu/neumont/projectFiles/models/RPSManager.java
@@ -0,0 +1,74 @@
+package edu.neumont.projectFiles.models;
+
+import edu.neumont.projectFiles.controllers.routing.games.rps.models.RPSMove;
+import utils.Tuple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bwaite on 5/27/2015.
+ */
+
+public class RPSManager{
+
+    public class RPSUser {
+        private UserModel user;
+        private int index;
+        private boolean doneWithGame;
+
+        public RPSUser(UserModel user) {
+            this.user = user;
+            this.index = 0;
+            this.doneWithGame = false;
+        }
+
+        public UserModel getUser() {
+            return user;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public boolean isDoneWithGame() {
+            return doneWithGame;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public void setDoneWithGame(boolean doneWithGame) {
+            this.doneWithGame = doneWithGame;
+        }
+    }
+
+    private RPSUser p1,p2;
+    //First = use p1 index, //Second = p2 index
+    private List<Tuple<RPSMove,RPSMove>> Moves = new ArrayList<>();
+
+    public RPSUser getP1() {
+        return p1;
+    }
+
+    public void setP1(UserModel user) {
+        this.p1 = new RPSUser(user);
+    }
+
+    public RPSUser getP2() {
+        return p2;
+    }
+
+    public void setP2(UserModel user) {
+        this.p2 = new RPSUser(user);
+    }
+
+    public List<Tuple<RPSMove, RPSMove>> getMoves() {
+        return Moves;
+    }
+
+    public void addMove(Tuple<RPSMove, RPSMove> move) {
+        this.Moves.add(move);
+    }
+}
